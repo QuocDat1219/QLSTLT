8f555bc932bbabe180ea964a03a6c1b9a0e6a846
thuan <lapthuan0805@gmail.com>
2023-11-25 17:02:44 +0700

    new api

--- controller/chiTietDonHangController.js
+++ controller/chiTietDonHangController.js
@@ -5,28 +5,17 @@ const { checkUpdate, checkInsert } = require("../auth/checkInfo");
 const getAllCtdonhang = async (req, res) => {
   try {
     const oracleQuery =
-      `select dh.MaPN as MaPN, TenHH, SoLuong, GiaNhap,dh.GiaBan as GiaBan, ThanhTien, DVT, TenST, HoTen,NgayNhap  from  ctphieunhapkho dh
-      inner join phieunhapkho ct on dh.MaPN = ct.MaPN 
-      inner join nhanvien nv on nv.MaNV = ct.MaNV 
-      inner join hanghoa hh on hh.MaHH = dh.mahh
-      inner join danhmuchh dm on hh.DanhMuc = dm.MaDanhMuc
-      inner join nhanhang nh on hh.MaNhanHang = nh.manhanhang
-      inner join sieuthi st on st.mast = nv.mast`;
+      `select *  from  ctdonhang `;
     const result = await executeOracleQuery(oracleQuery);
     const rows = result.rows;
     const jsonData = rows.map((row) => {
       return {
         MaDH: row[0],
-        TenKH: row[1],
-        HangHoa: row[2],
-        SL: row[3],
-        GiaBan: row[4],
-        MucGiam: row[5],
-        ThanhTien: row[6],
-        TenST: row[7],
-        HoTen: row[8],
-        ThanhToan: row[9],
-        NgayDHL: row[10],
+        MaHH: row[1],
+        SL: row[2],
+        DVT: row[3],
+        ThanhTien: row[4],
+        TenHH: row[5],
       };
     });
     res.json(jsonData);
@@ -38,7 +27,7 @@ const getAllCtdonhang = async (req, res) => {
 
 const getCtdonhangById = async (req, res) => {
   try {
-    const oracleQuery = `SELECT * FROM ctdonhang WHERE MaDH = '${req.params.id}'`;
+    const oracleQuery = `SELECT ct.*, hh.TenHH FROM ctdonhang ct , hanghoa hh  WHERE ct.MaHH = hh.MaHH and MaDH = '${req.params.id}'`;
     const result = await executeOracleQuery(oracleQuery);
     const rows = result.rows;
     const jsonData = rows.map((row) => {
@@ -48,6 +37,7 @@ const getCtdonhangById = async (req, res) => {
         SL: row[2],
         DVT: row[3],
         ThanhTien: row[4],
+        TenHH: row[5],
       };
     });
     res.json(jsonData);
@@ -110,24 +100,18 @@ const addCtdonhang = async (req, res) => {
 };
 
 const updateCtdonhang = async (req, res) => {
-  const { MaDH, MaHH, SL, DVT, ThanhTien } = req.body;
+  const { MaDH, MaHH, SL, DVT, ThanhTien,idMaHH } = req.body;
   const oracleQuery =
-    "UPDATE ctdonhang SET SL = :1, DVT = :2, ThanhTien = :3 WHERE MaDH = :4 AND MaHH = :5";
-  const mySqlQuery = `UPDATE ctdonhang SET SL = '${SL}', DVT = N'${DVT}', ThanhTien = '${ThanhTien}' WHERE MaDH = '${MaDH}' AND MaHH = '${MaHH}'`;
-  const checkCtdonhang = `SELECT COUNT(*) as count FROM ctdonhang WHERE MaDH = '${MaDH}' AND MaHH = '${MaHH}'`;
-
+    `UPDATE ctdonhang SET SL = :1, DVT = :2, ThanhTien = :3 , MaHH = :4  WHERE MaDH = '${MaDH}' AND MaHH = '${idMaHH}'`;
+  const mySqlQuery = `UPDATE ctdonhang SET  MaHH='${MaHH}', SL = '${SL}', DVT = N'${DVT}', ThanhTien = '${ThanhTien}' WHERE MaDH = '${MaDH}' AND MaHH = '${idMaHH}'`;
+console.log(mySqlQuery)
   try {
-    const recordExists = await checkUpdate(checkCtdonhang);
-    if (!recordExists) {
-      res.status(404).json({ message: "Không tìm thấy bản ghi để cập nhật" });
-      return;
-    }
-
+  
     mysqlConnection.query(mySqlQuery, (mysqlError) => {
       if (mysqlError) {
         res.status(500).json({ message: "Lỗi khi cập nhật ở MySQL" });
       } else {
-        executeOracleQuery(oracleQuery, [SL, DVT, ThanhTien, MaDH, MaHH]);
+        executeOracleQuery(oracleQuery, [SL, DVT, ThanhTien, MaHH]);
         res.json({ message: "Cập nhật chi tiết đơn hàng thành công" });
       }
     });
@@ -138,7 +122,7 @@ const updateCtdonhang = async (req, res) => {
 };
 
 const deleteCtdonhang = async (req, res) => {
-  const { MaDH, MaHH } = req.params;
+  const { MaDH, MaHH } = req.body;
   const deleteQuery = `DELETE FROM ctdonhang WHERE MaDH = '${MaDH}' AND MaHH = '${MaHH}'`;
   const checkCtdonhang = `SELECT cOUNT(*) as count FROM ctdonhang WHERE MaDH = '${MaDH}' AND MaHH = '${MaHH}'`;
   try {
